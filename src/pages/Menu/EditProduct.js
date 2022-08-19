e6432365ccde63e34ac3134c6590d7134bfb03f8
OmarDraz <omarnader6636@gmail.com>
2022-08-19 15:35:03 +0200

    env

--- src/pages/Menu/EditProduct.js
+++ src/pages/Menu/EditProduct.js
@@ -116,7 +116,7 @@ const EditProduct = () => {
                 <label for="name">صورة المنتج</label> &nbsp;
                 <input type="file" name="image" onChange={handleImage} id="name" />
             </div>
-            <p>الحالي <a rel="noreferrer" target="_blank" href={currentImg}>{currentImg.split('${process.env.HOST}/uploads/')}</a></p>
+            <p>الحالي <a rel="noreferrer" target="_blank" href={currentImg}>{currentImg.split(`${process.env.REACT_APP_HOST_API}/uploads/`)}</a></p>
         </div>
         <div className="col-6 col-sm-12">
             <div className="form-group">
